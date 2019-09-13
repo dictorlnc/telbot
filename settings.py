47db9e3cced12a73ce65e5d543b07f87fe09bbca
Who am i <49260390+dictorlnc@users.noreply.github.com>
2019-09-13 19:36:38 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+
+token = "985371595:AAE8n1xXcmjY7AXO9yZl8kNZsMNUB-JMMIE"
+
+admins = [@Dictor_lnc] #админы бота через запятую
+
+LITTLE_SUBCOIN_TO_GET_SUBS = 5 #минимум сабкоинов для раскрутки канала
+
+REF_BONUS = 2 #реферальный бонус (сабкоины)
+
+FINE_FOR_UNSUBSCRIBING = -5 #Число должно быть отрицательным и целым
+
+SUBSCRIPTION_TERM = 7 #Какое время юзер должен оьязательно быть подписан на канал?
+
+LINK_TO_INTRODUCTION_AND_RULES = 'https://telegra.ph/Vvedenie-v-bota-a-takzhe-pravila-06-09' #ссылка на правила и инструкцию
+
+LINK_TO_CHAT_OF_BOT = '@MoneyCoin_bot' #ссылка на чат
